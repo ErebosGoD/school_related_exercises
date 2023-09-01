128f71426e273922b6044e69b2dbc196848ac413
Erebos <boehm.pascal98@gmail.com>
2023-09-01 18:03:48 +0200

    current wip 01-09-2023

--- guessthenumber/restapi.py
+++ guessthenumber/restapi.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, url_for
 from usermanager import UserManager
+from gamemanager import GameManager
 
 
 app = Flask(__name__, template_folder='.')
+
+users = {}
+attempts = {}
 
 
 @app.route('/')
@@ -10,21 +14,28 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST'])
 def login():
-    args = request.args.to_dict()
-    username = args['username']
+    username = request.form.get('username')
+    print(username)
     user_manager.create_user(username)
-    return redirect('/game')
+    return redirect(url_for('game', name=username))
 
 
-@app.route('/game')
-def game():
-    return render_template('game.html')
+@app.route('/game/<name>')
+def game(name):
+    random_number = game_manager.get_random_number()
+    return render_template('game.html', username=name, random_number=random_number)
+
+
+@app.route('/check-guess')
+def check_guess():
+    guess = int(request.form.get('guess'))
 
 
 if __name__ == '__main__':
     database_path = "guessthenumber\database.db"
     user_manager = UserManager(database_path)
     user_manager._create_tables()
+    game_manager = GameManager()
     app.run()
